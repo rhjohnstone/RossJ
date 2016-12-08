b63005d260b9d4513e60469ac719592ad6019d7b
Ross Johnstone <rhjohnstone@gmail.com>
2016-12-08 15:08:51 +0000

    Synthetic hierarchical MCMC working again

--- src/python/test_hierarchical_mcmc.py
+++ src/python/test_hierarchical_mcmc.py
@@ -29,11 +29,21 @@ def log_pi_theta_i(theta_i,theta,sigma_squareds,sigma,data_i,test_i):
     sum_2 = np.sum(((theta_i-theta)**2)/sigma_squareds)
     return -0.5 * (sum_1 + sum_2)
 
-def log_pi_sigma(expt_datas,test_datas,sigma,N_e,num_pts,uniform_noise_prior):
+def log_pi_sigma(expt_datas,test_datas,sigma,num_expts,num_pts,uniform_noise_prior):
     if (not (uniform_noise_prior[0] < sigma < uniform_noise_prior[1])):
         return -np.inf
     else:
-        return -N_e*num_pts*np.log(sigma) - np.sum((expt_datas-test_datas)**2) / (2*sigma**2)
+        return -num_expts*num_pts*np.log(sigma) - np.sum((expt_datas-test_datas)**2) / (2*sigma**2)
+        
+#if t > 1000*number_of_parameters:
+def update_covariance_matrix(t,thetaCur,meanum_exptsstimate,cov_estimate,loga,accepted,when_to_adapt):
+    s = t - when_to_adapt
+    gamma_s = 1/(s+1)**0.6
+    temp_covariance_bit = np.array([thetaCur-meanum_exptsstimate])
+    new_cov_estimate = (1-gamma_s) * cov_estimate + gamma_s * np.dot(np.transpose(temp_covariance_bit),temp_covariance_bit)
+    new_meanum_exptsstimate = (1-gamma_s) * meanum_exptsstimate + gamma_s * thetaCur
+    new_loga = loga + gamma_s*(accepted-0.25)
+    return new_cov_estimate, new_meanum_exptsstimate, new_loga
 
 model = int(sys.argv[1])
 protocol = 1
@@ -41,6 +51,8 @@ python_seed = 1
 noise_sd = 0.25
 
 num_expts = 5
+
+uniform_noise_prior = [0,50]
 
 npr.seed(python_seed)
 
@@ -67,7 +79,6 @@ with open(chain_file,"w") as outfile:
     outfile.write("# noise_sd {}\n".format(noise_sd))
 
 
-python_seed = python_seed+1
 npr.seed(python_seed)
 
 def normal_loglikelihood_uniform_priors(test_trace,expt_trace,sigma):
@@ -75,7 +86,6 @@ def normal_loglikelihood_uniform_priors(test_trace,expt_trace,sigma):
     sum_of_square_diffs = np.sum((test_trace-expt_trace)**2)
     return -num_pts*np.log(sigma)-sum_of_square_diffs/(2.*sigma**2)
 
-chain_file, figs_dir = ms.synthetic_nonhierarchical_chain_file_and_figs_dir(model,protocol,python_seed)
 
 # need to make sure these are always same as in C++ protocol
 # might have to change it to have protocols hard-coded into Python instead of C++
@@ -89,16 +99,16 @@ cell = ap.APSimulator()
 cell.DefineProtocol(protocol)
 cell.DefineModel(model)
 
-expt_traces = []
+expt_traces = np.zeros((num_expts,num_pts))
 fig = plt.figure()
 ax = fig.add_subplot(111)
 for i in xrange(num_expts):
     temp_trace = cell.SolveForVoltageTraceWithParams(expt_params[i,:])
     temp_trace += noise_sd*npr.randn(len(temp_trace))
-    expt_traces.append(temp_trace)
+    expt_traces[i,:] = temp_trace
     ax.plot(expt_times,temp_trace)
 print expt_traces
-plt.show(block=True)
+plt.close()
 
 initial_theta_curs = np.copy(expt_params)
 
@@ -123,13 +133,16 @@ top_theta_cur[np.where(top_theta_cur<0)] = 0.
 top_sigma_squareds_cur[np.where(top_sigma_squareds_cur<0)] = 0.
 theta_is_cur[np.where(theta_is_cur<0)] = 0.
 
+means = np.copy(theta_is_cur)
+print "means:\n", means, "\n"
+
 first_iteration = np.concatenate((top_theta_cur,top_sigma_squareds_cur,theta_is_cur.flatten(),[noise_sigma_cur])) # not including likelihoods
 num_all_params = len(first_iteration)
 
 
-temp_test_traces = [cell.SolveForVoltageTraceWithParams(params) for params in theta_is_cur]
+temp_test_traces_cur = [cell.SolveForVoltageTraceWithParams(params) for params in theta_is_cur]
 
-print temp_test_traces
+print temp_test_traces_cur
 
 proposal_scale = 0.001
 
@@ -137,7 +150,7 @@ covariances = [proposal_scale*np.eye(num_g_params)*original_gs for i in xrange(n
 
 print covariances
 
-sys.exit()
+#sys.exit()
 
 logas = [0.]*num_expts
 acceptances = [0.]*num_expts
@@ -145,7 +158,9 @@ sigma_loga = 0.
 sigma_acceptance = 0.
 
 acceptance_target = 0.25
-when_to_adapt = 100*num_params
+when_to_adapt = 20*num_g_params
+
+
 
 num_total_iterations = 10000
 thinning = 5
@@ -164,72 +179,103 @@ ax.set_ylabel('Membrane voltage (mV)')
 ax.set_title('All (post-burn, thinned) MCMC sample traces')
 
 burn = num_saved_iterations/4
-mcmc = np.zeros((num_saved_iterations+1,num_all_params))
+mcmc = np.zeros((num_saved_iterations+1,num_all_params)) # when do I need to worry about memory?
 mcmc[0,:] = first_iteration
 start = time.time()
 for it in xrange(1,num_total_iterations+1):
-    accepted = 0.
-    theta_star = npr.multivariate_normal(theta_cur,np.exp(loga)*cov_estimate)
-    if np.all(theta_star>0):
-        test_trace = cell.SolveForVoltageTraceWithParams(theta_star[:-1])
-        log_target_star = normal_loglikelihood_uniform_priors(test_trace,expt_trace,theta_star[-1])
-        alpha = npr.rand()
-        if ( np.log(alpha) < log_target_star-log_target_cur ):
-            theta_cur = theta_star
-            log_target_cur = log_target_star
-            accepted = 1.
-    if (it%thinning==0):
-        mcmc[it/thinning,:] = np.concatenate((theta_cur,[log_target_cur]))
-        if (it>=burn):
-            ax.plot(expt_times,test_trace,alpha=0.01)
-    if (it>when_to_adapt):
-        s = it - when_to_adapt
-        gamma_s = 1./(s+1.)**0.6
-        temp_covariance_bit = np.array([theta_cur-mean_estimate])
-        cov_estimate = (1-gamma_s) * cov_estimate + gamma_s * temp_covariance_bit.T.dot(temp_covariance_bit)
-        mean_estimate = (1-gamma_s) * mean_estimate + gamma_s * theta_cur
-        loga += gamma_s*(accepted-acceptance_target)
-    acceptance = ((it-1.)*acceptance + accepted)/it
-    if (it%when_to_update==0):
+    for j in xrange(num_g_params):
+        temp_eta = new_eta(old_eta_js[j,:],theta_is_cur[:,j])
+        while True:
+            temp_top_theta_cur, temp_top_sigma_squared_cur = sample_from_N_IG(temp_eta)
+            if (temp_top_theta_cur > 0):
+                top_theta_cur[j] = np.copy(temp_top_theta_cur)
+                top_sigma_squareds_cur[j] = np.copy(temp_top_sigma_squared_cur)
+                break
+                
+    # theta i's for each experiment
+    for i in range(num_expts):
+        while True:
+            theta_i_star = npr.multivariate_normal(theta_is_cur[i,:],np.exp(logas[i])*covariances[i])
+            if (np.all(theta_i_star>=0)):
+                break
+        temp_test_trace_star = cell.SolveForVoltageTraceWithParams(theta_i_star)
+        target_cur = log_pi_theta_i(theta_is_cur[i,:],top_theta_cur,top_sigma_squareds_cur,noise_sigma_cur,expt_traces[i],temp_test_traces_cur[i])
+        target_star = log_pi_theta_i(theta_i_star,top_theta_cur,top_sigma_squareds_cur,noise_sigma_cur,expt_traces[i],temp_test_trace_star)
+        u = npr.rand()
+        if (np.log(u) < target_star - target_cur):
+            theta_is_cur[i,:] = np.copy(theta_i_star)
+            temp_test_traces_cur[i] = temp_test_trace_star
+            accepted = 1
+        else:
+            accepted = 0
+        if (it > when_to_adapt):
+            covariances[i],means[i],logas[i] = update_covariance_matrix(it,theta_is_cur[i,:],means[i],covariances[i],logas[i],accepted,when_to_adapt)
+        acceptances[i] = (it*acceptances[i] + accepted)/(it+1)
+    # noise sigma
+    noise_sigma_star = noise_sigma_cur + np.exp(sigma_loga)*proposal_scale*npr.randn()
+    
+    target_star = log_pi_sigma(expt_traces,temp_test_traces_cur,noise_sigma_star,num_expts,num_pts,uniform_noise_prior)
+    target_cur = log_pi_sigma(expt_traces,temp_test_traces_cur,noise_sigma_cur,num_expts,num_pts,uniform_noise_prior)
+    
+    
+    
+    
+    u = npr.rand()
+    if (np.log(u) < target_star - target_cur):
+        noise_sigma_cur = noise_sigma_star
+        accepted = 1
+    else:
+        accepted = 0
+    sigma_acceptance = (it*sigma_acceptance + accepted)/(it+1)
+    if (it > when_to_adapt):
+        r = it - when_to_adapt
+        gamma_r = 1/(r+1)**0.6
+        sigma_loga += gamma_r*(accepted-acceptance_target)
+    if ( it%thinning == 0 ):
+        mcmc[it/thinning,:] = np.concatenate((top_theta_cur,top_sigma_squareds_cur,theta_is_cur.flatten(),[noise_sigma_cur]))
+
+    if ( it%when_to_update==0 ):
         time_taken_so_far = time.time()-start
         estimated_time_left = time_taken_so_far/it*(num_total_iterations-it)
         print it/when_to_update, "/", how_many_updates
         print "Time taken: {} s = {} min".format(round(time_taken_so_far,1),round(time_taken_so_far/60.,2))
-        print "acceptance:", acceptance
-        print "loga:", loga
+        print "acceptances:", acceptances
+        print "logas:", logas
         print "Estimated time remaining: {} s = {} min".format(round(estimated_time_left,1),round(estimated_time_left/60.,2))
 time_taken = time.time()-start
 print "Time taken: {} s".format(round(time_taken,2))
+print mcmc
 
-ax.plot(expt_times,expt_trace,color='red',label='Synthetic expt')
-ax.legend()
-fig.tight_layout()
-fig.savefig(figs_dir+'mcmc_sample_traces.png')
-plt.close()
+print "\nShould be saved to", chain_file, "\n"
+
     
 with open(chain_file,"a") as outfile:
     np.savetxt(outfile,mcmc)
     
 
-
-for i in xrange(num_params):
-    if (i<num_params-1):
-        original_value = original_gs[i]
+for expt in xrange(num_expts):
+    for i in xrange(num_g_params):
+        original_value = expt_params[expt,i]
         label = g_parameters[i]
-    elif (i==num_params-1):
-        original_value = noise_sd
-        label = r"\sigma"
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.grid()
-    ax.hist(mcmc[burn:,i],normed=True,bins=40,color='blue',edgecolor='blue')
-    ax.axvline(original_value,color='red',lw=2)
-    ax.set_ylabel('Probability density')
-    ax.set_xlabel('$'+label+'$')
-    fig.tight_layout()
-    fig.savefig(figs_dir+label.translate(None,r'\\{}')+'_marginal.png') # need double slashes to get rid of the sigma slash for some reason
-    plt.close()
+        param_index = (2+expt)*num_g_params+i
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.grid()
+        ax.hist(mcmc[burn:,param_index],normed=True,bins=40,color='blue',edgecolor='blue')
+        ax.axvline(original_value,color='red',lw=2)
+        ax.set_ylabel('Probability density')
+        ax.set_xlabel('Expt {}: $'.format(expt+1)+label+'$')
+        fig.tight_layout()
+        fig.savefig(figs_dir+"expt_{}_".format(expt+1)+label.translate(None,r'\\{}')+'_marginal.png') # need double slashes to get rid of the sigma slash for some reason
+        plt.close()
 
-
-
-
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.grid()
+ax.hist(mcmc[burn:,-1],normed=True,bins=40,color='blue',edgecolor='blue')
+ax.axvline(noise_sd,color='red',lw=2)
+ax.set_ylabel('Probability density')
+ax.set_xlabel(r'$\sigma$')
+fig.tight_layout()
+fig.savefig(figs_dir+'sigma_marginal.png') # need double slashes to get rid of the sigma slash for some reason
+plt.close()
