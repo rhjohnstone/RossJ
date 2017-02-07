613e7bec0cbfbcdc582977847c41f7ece4dfb0d4
Ross Johnstone <rhjohnstone@gmail.com>
2017-02-07 16:29:52 +0000

    Normalised traces

--- src/python/fit_with_stim.py
+++ src/python/fit_with_stim.py
@@ -14,13 +14,20 @@ import itertools as it
 def prior_upper_bounds(original_gs):
     return 100*np.array(original_gs)
 
-
+def normalise_trace(trace):
+    trace_min = np.min(trace)
+    trace_max = np.max(trace)
+    scale = trace_max - trace_min
+    scaled_trace = (trace-trace_min) / scale
+    return scaled_trace
     
 
 temp_dog_AP_file = "projects/RossJ/python/input/ken/036-2014091101 Control 1Hz analyzed.txt_averagedTrace.txt"
 dog_AP = np.loadtxt(temp_dog_AP_file)#,delimiter=',')
 expt_times = dog_AP[:,0]
 expt_trace = dog_AP[:,1]
+
+scaled_expt_trace = normalise_trace(expt_trace)
 
 print expt_times
 print expt_trace
@@ -77,6 +84,15 @@ def sum_of_square_diffs(params):#,expt_trace,upper_bounds,ap):
         return np.inf
     test_trace = ap.SolveForVoltageTraceWithParams(params)
     return np.sum((test_trace-expt_trace)**2)
+    
+def normalised_sum_of_square_diffs(params):#,expt_trace,upper_bounds,ap):
+    if np.any(params<0) or np.any(params>upper_bounds):
+        #print test_gs
+        return np.inf
+    scaled_test_trace = normalise_trace(ap.SolveForVoltageTraceWithParams(params))
+    return np.sum((scaled_test_trace-scaled_expt_trace)**2)
+
+
 
 original_obj_fun = sum_of_square_diffs(x0)#,expt_trace,upper_bounds,ap)
 print "original_obj_fun =", original_obj_fun
@@ -92,7 +108,8 @@ sigma0 = 0.00001
 es = cma.CMAEvolutionStrategy(x0, sigma0, opts)
 while not es.stop():
     X = es.ask()
-    f_vals = pool.map_async(sum_of_square_diffs,X).get(9999)
+    #f_vals = pool.map_async(sum_of_square_diffs,X).get(9999)
+    f_vals = pool.map_async(normalised_sum_of_square_diffs,X).get(9999)
     #temp_vals = [sum_of_square_diffs(x,expt_trace,upper_bounds,ap) for x in X]
     es.tell(X, f_vals)
     es.disp()
@@ -101,25 +118,30 @@ res = es.result()
 time_taken = time.time()-start
 print "res[0] =", res[0]
 best_gs = res[0]
+best_f = res[1]
 
 print "\nTime taken: {} s\n".format(round(time_taken,1))
 
-best_fit_trace = ap.SolveForVoltageTraceWithParams(best_gs)
+best_fit_trace = normalise_trace(ap.SolveForVoltageTraceWithParams(best_gs))
 
 print "original_gs:", original_gs
 print "best_gs:", best_gs
 
 #print original_gs/best_gs
 
-true_trace = ap.SolveForVoltageTraceWithParams(original_gs)
+#true_trace = ap.SolveForVoltageTraceWithParams(original_gs)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.grid()
-ax.plot(expt_times,expt_trace,color='red', label='Expt')
-ax.plot(times,true_trace,color='blue',label='Original')
+ax.plot(expt_times,scaled_expt_trace,color='red', label='Expt')
+#ax.plot(times,true_trace,color='blue',label='Original')
 ax.plot(times,best_fit_trace,color='green',label='Best fit')
 ax.legend()
 fig.tight_layout()
-fig.savefig("ken_trace_fit_to_model_{}.png".format(model_number))
+fig.savefig("ken_normalised_trace_fit_to_model_{}.png".format(model_number))
 plt.close()
+
+params_file = "ken_normalised_best_fit_params_model_{}.txt".format(model_number)
+
+np.savetxt(params_file,np.concatenate((best_gs,[best_f])))
