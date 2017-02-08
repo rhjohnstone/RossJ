2baf8fd4eb5deef6f3836ebf2d928500ada29b1b
Ross Johnstone <rhjohnstone@gmail.com>
2017-02-08 15:53:29 +0100

    Save params on new lines

--- src/python/fit_with_stim.py
+++ src/python/fit_with_stim.py
@@ -162,3 +162,4 @@ params_file = "ken_best_fit_params_model_{}.txt".format(model_number)
 
 with open(params_file,'a') as outfile:
     np.savetxt(outfile,np.concatenate((best_gs,[best_f])), newline=" ")
+    outfile.write("\n")
