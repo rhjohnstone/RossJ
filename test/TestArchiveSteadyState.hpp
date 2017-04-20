1b27294e0bcba726741f4c9e8dcf17c2c7c82452
Ross Johnstone <rhjohnstone@gmail.com>
2017-04-20 22:55:59 +0100

    Archive SS variables

--- test/TestArchiveSteadyState.hpp
+++ test/TestArchiveSteadyState.hpp
@@ -9,7 +9,7 @@
 class TestArchiveSteadyState : public CxxTest::TestSuite
 {
 public:
-    void TestRunToSteadyState() throw(Exception)
+    void xTestRunToSteadyState() throw(Exception)
     {
 #ifdef CHASTE_CVODE
 
@@ -38,7 +38,7 @@ public:
         std::cout << "Cvode is not enabled.\n";
 #endif
     }
-    void TestArchiveVariables() throw(Exception)
+    void xTestArchiveNonSSVariables() throw(Exception)
     {
 #ifdef CHASTE_CVODE
 
@@ -60,7 +60,31 @@ public:
         std::cout << "Cvode is not enabled.\n";
 #endif
     }
+    void TestArchiveSSVariables() throw(Exception)
+    {
+#ifdef CHASTE_CVODE
+
+        double stimulus_magnitude = -25.5, stimulus_duration = 2, stimulus_period = 1000, stimulus_start_time = 20;
+        
+        double solve_start = 0, solve_end = 400, solve_timestep = 0.1;
     
+        unsigned model_number = 8u; // 8. Decker 2009 dog
+    
+        APSimulator simulator;
+        
+        simulator.DefineStimulus( stimulus_magnitude, stimulus_duration, stimulus_period, stimulus_start_time );
+        simulator.DefineSolveTimes(solve_start, solve_end, solve_timestep);
+        simulator.DefineModel(model_number);
+        
+        bool result = simulator.RunToSteadyState();
+        TS_ASSERT_EQUALS(result,true);
+        
+        simulator.ArchiveStateVariables();
+
+#else
+        std::cout << "Cvode is not enabled.\n";
+#endif
+    }
 };
 
 #endif /*TESTARCHIVESTEADYSTATE_HPP_*/
