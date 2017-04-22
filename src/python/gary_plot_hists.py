5083f41693af84dc6aa4c4935e3a6ad60b1d0066
Ross Johnstone <rhjohnstone@gmail.com>
2017-04-22 22:34:15 +0100

    Plot hists and generating distribution

--- src/python/gary_plot_hists.py
+++ src/python/gary_plot_hists.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mcmc_setup as ms
 from glob import glob
+from scipy.stats import norm
 
 model_number = 8
 original_gs, g_parameters = ms.get_original_params(model_number)
@@ -24,15 +25,20 @@ fit_expt_params = expt_params[sorted_inds,:]
 best_fit_params = np.zeros((num_expts, num_params))
 for i in xrange(num_expts):
     best_fit_params[i, :] = np.loadtxt(best_fit_params_files[i])
-print best_fit_params
 
+num_pts = 201
 for p in xrange(num_params):
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.hist(fit_expt_params[:, p], normed=True, bins=10, color='blue', edgecolor=None, alpha=0.5, label="Expt")
-    ax1.hist(best_fit_params[:, p], normed=True, bins=10, color='red', edgecolor=None, alpha=0.5, label="Best fit")
+    ax1.hist(fit_expt_params[:, p], normed=True, bins=10, color='blue', edgecolor=None, alpha=0.5, label="Expt", zorder=10)
+    ax1.hist(best_fit_params[:, p], normed=True, bins=10, color='red', edgecolor=None, alpha=0.5, label="Best fit", zorder=11)
+    ax1.axvline(original_gs[p],color='orange')
     ax1.legend()
-    ax1.set_xlabel(g_parameters[i])
+    ax1.set_xlabel(g_parameters[p])
     ax1.set_ylabel("Normalised histogram")
+    x = ax1.get_xlim()
+    pdf_x = np.linspace(x[0], x[1], num_pts)
+    pdf_y = norm.pdf(pdf_x, loc=1.1*original_gs[p], scale=0.15*original_gs[p])
+    ax1.plot(pdf_x, pdf_y, color='green', label="Expt generating")
     fig.tight_layout()
-plt.show()
+    plt.show()
