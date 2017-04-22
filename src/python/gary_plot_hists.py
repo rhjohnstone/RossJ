417f1de93302ed8f605eec3797be89a1f38caf92
Ross Johnstone <rhjohnstone@gmail.com>
2017-04-22 22:09:59 +0100

    Sorted expt files, probably not worth it

--- src/python/gary_plot_hists.py
+++ src/python/gary_plot_hists.py
@@ -5,16 +5,19 @@ import matplotlib.pyplot as plt
 from glob import glob
 
 params_file = "gary_decker_params.txt"
-num_params = params_file.shape[1]
 expt_params = np.loadtxt(params_file)
+num_params = expt_params.shape[1]
 
-best_fit_params_files = sorted(glob("gary_decker_expt_*_best_fit_params.txt"))
-print best_fit_params_files
+best_fit_params_files = glob("gary_decker_expt_*_best_fit_params.txt")
 expts = [int(x.split("_")[3]) for x in best_fit_params_files]
+sorted_inds = np.argsort(expts)
+best_fit_params_files = best_fit_params_files[sorted_inds]
+expts = expts[sorted_inds]
 num_expts = len(expts)
 print expts
+print best_fit_params_files
 
 best_fit_params = np.zeros((num_expts, num_params))
 for i in xrange(num_expts):
-    best_fit_params[i, :] = np.loadtxt(best_fit_params_files[expts[i]])
+    best_fit_params[i, :] = np.loadtxt(best_fit_params_files[i])
 print best_fit_params
